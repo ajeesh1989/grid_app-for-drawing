9a906e12531f77ac1800b1aaf09dde356a0b07b3
ajeesh1989 <ajeeshrko@gmail.com>
2023-09-12 22:11:06 +0530

    crop,save to gallery, share added

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_colorpicker/flutter_colorpicker.dart';
+import 'package:image_cropper/image_cropper.dart';
+import 'package:image_picker/image_picker.dart';
+import 'package:share_plus/share_plus.dart';
+import 'dart:io';
+import 'package:image_gallery_saver/image_gallery_saver.dart';
 
 void main() => runApp(const MyApp());
 
@@ -17,51 +22,26 @@ class MyApp extends StatelessWidget {
 }
 
 class GridApp extends StatefulWidget {
-  const GridApp({super.key});
+  const GridApp({Key? key}) : super(key: key);
 
   @override
   _GridAppState createState() => _GridAppState();
 }
 
 class _GridAppState extends State<GridApp> {
-  int rows = 5;
-  int columns = 5;
+  int rows = 3; // Changed to fixed number of rows
+  int columns = 3; // Changed to fixed number of columns
   Color gridColor = Colors.black;
   bool showNumbers = true;
   double lineWidth = 1.0;
+  File? imagePath;
+  CroppedFile? _croppedFile;
+  bool showGrid = false; // Added to control grid visibility
 
   void changeGridColor(Color color) {
     setState(() {
       gridColor = color;
     });
-  }
-
-  void increaseRows() {
-    setState(() {
-      rows++;
-    });
-  }
-
-  void decreaseRows() {
-    if (rows > 1) {
-      setState(() {
-        rows--;
-      });
-    }
-  }
-
-  void increaseColumns() {
-    setState(() {
-      columns++;
-    });
-  }
-
-  void decreaseColumns() {
-    if (columns > 1) {
-      setState(() {
-        columns--;
-      });
-    }
   }
 
   void toggleShowNumbers() {
@@ -84,14 +64,186 @@ class _GridAppState extends State<GridApp> {
     }
   }
 
-  void resetApp() {
+  void increaseRows() {
     setState(() {
-      rows = 3;
-      columns = 3;
-      gridColor = Colors.black;
-      showNumbers = true;
-      lineWidth = 1.0;
+      rows++;
+      columns++;
     });
+  }
+
+  void decreaseRows() {
+    if (rows > 1) {
+      setState(() {
+        rows--;
+        columns--;
+      });
+    }
+  }
+
+  void resetImageAndGrid() {
+    setState(() {
+      imagePath = null;
+      showGrid = false; // Hide the grid
+    });
+  }
+
+  Future<void> pickImage({required ImageSource imageSource}) async {
+    final image = await ImagePicker().pickImage(source: imageSource);
+    if (image != null) {
+      final file = File(image.path);
+      setState(() {
+        imagePath = file;
+        showGrid = true; // Show the grid when an image is selected
+      });
+      final img = await _cropImage(imagefile: file);
+
+      setState(() {
+        imagePath = img;
+      });
+    }
+  }
+
+  Future<File?> _cropImage({required File imagefile}) async {
+    if (imagePath != null) {
+      final croppedFile = await ImageCropper().cropImage(
+        sourcePath: imagefile.path,
+        compressFormat: ImageCompressFormat.jpg,
+        compressQuality: 100,
+        uiSettings: [
+          AndroidUiSettings(
+            toolbarTitle: 'Cropper',
+            toolbarColor: Colors.blue,
+            toolbarWidgetColor: Colors.white,
+            initAspectRatio: CropAspectRatioPreset.original,
+            lockAspectRatio: false,
+          ),
+          IOSUiSettings(
+            title: 'Cropper',
+          ),
+          WebUiSettings(
+            context: context,
+            presentStyle: CropperPresentStyle.dialog,
+            boundary: const CroppieBoundary(
+              width: 520,
+              height: 520,
+            ),
+            viewPort: const CroppieViewPort(
+              width: 480,
+              height: 480,
+              type: 'circle',
+            ),
+            enableExif: true,
+            enableZoom: true,
+            showZoomer: true,
+          ),
+        ],
+      );
+      return File(croppedFile!.path);
+    }
+    return null;
+  }
+
+  void showImageSourceSelection() {
+    showModalBottomSheet(
+      context: context,
+      shape: const RoundedRectangleBorder(
+        borderRadius: BorderRadius.vertical(top: Radius.circular(30.0)),
+      ),
+      builder: (BuildContext context) {
+        return ClipRRect(
+          borderRadius: const BorderRadius.vertical(top: Radius.circular(30.0)),
+          child: Container(
+            padding: const EdgeInsets.all(16),
+            child: Column(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                const SizedBox(height: 10),
+                Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                  ),
+                  elevation: 8,
+                  child: SizedBox(
+                    height: 50,
+                    child: GestureDetector(
+                      onTap: () async {
+                        await pickImage(imageSource: ImageSource.gallery);
+                        // ignore: use_build_context_synchronously
+                        Navigator.pop(context);
+                      },
+                      child: const Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(Icons.image),
+                          SizedBox(width: 8),
+                          Text(
+                            'Browse Gallery',
+                            style: TextStyle(fontSize: 16),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 16),
+                const Text('OR'),
+                const SizedBox(height: 16),
+                Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(30.0),
+                  ),
+                  elevation: 8,
+                  child: SizedBox(
+                    height: 50,
+                    child: GestureDetector(
+                      onTap: () async {
+                        await pickImage(imageSource: ImageSource.camera);
+                        // ignore: use_build_context_synchronously
+                        Navigator.pop(context);
+                      },
+                      child: const Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Icon(Icons.camera_alt_outlined),
+                          SizedBox(width: 8),
+                          Text(
+                            'Use a Camera',
+                            style: TextStyle(fontSize: 16),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 60),
+              ],
+            ),
+          ),
+        );
+      },
+    );
+  }
+
+  void saveImageToGallery() async {
+    if (imagePath != null) {
+      final result = await ImageGallerySaver.saveFile(imagePath!.path);
+      print('Image saved to gallery: $result');
+      // ignore: use_build_context_synchronously
+      ScaffoldMessenger.of(context).showSnackBar(
+        const SnackBar(
+          content: Text('Image saved to gallery'),
+          duration: Duration(seconds: 3),
+        ),
+      );
+    }
+  }
+
+  void sendImageToWhatsApp() async {
+    if (imagePath != null) {
+      await Share.shareFiles([imagePath!.path], text: '');
+    } else {
+      throw 'No image selected';
+    }
   }
 
   @override
@@ -101,7 +253,7 @@ class _GridAppState extends State<GridApp> {
         title: const Text('Grid App'),
         actions: <Widget>[
           IconButton(
-            onPressed: resetApp,
+            onPressed: resetImageAndGrid,
             icon: const Icon(Icons.refresh),
           ),
         ],
@@ -110,25 +262,58 @@ class _GridAppState extends State<GridApp> {
         crossAxisAlignment: CrossAxisAlignment.center,
         children: <Widget>[
           Expanded(
-            child: GridView.builder(
-              itemCount: rows * columns,
-              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                crossAxisCount: columns,
-              ),
-              itemBuilder: (context, index) {
-                final number = showNumbers ? (index + 1).toString() : '';
-                return Container(
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      color: gridColor,
-                      width: lineWidth,
+            child: Stack(
+              alignment: Alignment.center,
+              children: [
+                if (imagePath != null)
+                  Image.file(
+                    imagePath!,
+                    fit: BoxFit.cover,
+                  ),
+                if (showGrid)
+                  GridView.builder(
+                    itemCount: rows * columns,
+                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                      crossAxisCount: columns,
+                    ),
+                    itemBuilder: (context, index) {
+                      final number = showNumbers ? (index + 1).toString() : '';
+                      return Container(
+                        decoration: BoxDecoration(
+                          border: Border.all(
+                            color: gridColor,
+                            width: lineWidth,
+                          ),
+                        ),
+                        child: Center(
+                          child: Text(number),
+                        ),
+                      );
+                    },
+                  ),
+                if (imagePath == null &&
+                    !showGrid) // Display message when nothing is selected
+                  Container(
+                    width: 80, // Adjust the width as needed
+                    height: 80, // Adjust the height as needed
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.grey.shade800, // Background color
+                    ),
+                    child: Center(
+                      child: IconButton(
+                        onPressed: () {
+                          showImageSourceSelection();
+                        },
+                        icon: const Icon(
+                          Icons.add,
+                          size: 30, // Adjust the size as needed
+                          color: Colors.white, // Icon color
+                        ),
+                      ),
                     ),
                   ),
-                  child: Center(
-                    child: Text(number),
-                  ),
-                );
-              },
+              ],
             ),
           ),
           Padding(
@@ -155,33 +340,6 @@ class _GridAppState extends State<GridApp> {
                           ),
                           IconButton(
                             onPressed: decreaseRows,
-                            icon:
-                                const Icon(Icons.minimize, color: Colors.black),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-                Container(
-                  color: Colors.blue.shade100,
-                  child: Column(
-                    children: [
-                      const SizedBox(
-                        height: 5,
-                      ),
-                      const Text(
-                        'Columns',
-                        style: TextStyle(color: Colors.black),
-                      ),
-                      Row(
-                        children: [
-                          IconButton(
-                            onPressed: increaseColumns,
-                            icon: const Icon(Icons.add, color: Colors.black),
-                          ),
-                          IconButton(
-                            onPressed: decreaseColumns,
                             icon:
                                 const Icon(Icons.minimize, color: Colors.black),
                           ),
@@ -259,6 +417,67 @@ class _GridAppState extends State<GridApp> {
           const SizedBox(
             height: 10,
           ),
+          if (imagePath != null)
+            Column(
+              children: [
+                Container(
+                  width: 150,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.black,
+                      width: 1,
+                    ),
+                  ),
+                  child: TextButton(
+                    onPressed: () {
+                      sendImageToWhatsApp();
+                    },
+                    child: const Center(
+                      child: Text('Sent to WhatsApp'),
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 20),
+                Container(
+                  width: 150,
+                  height: 50,
+                  decoration: BoxDecoration(
+                    border: Border.all(
+                      color: Colors.black,
+                      width: 1,
+                    ),
+                  ),
+                  child: TextButton(
+                    onPressed: () {
+                      saveImageToGallery();
+                    },
+                    child: const Center(
+                      child: Text('Save to Gallery'),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          const SizedBox(height: 20),
+          // Container(
+          //   width: 150,
+          //   height: 50,
+          //   decoration: BoxDecoration(
+          //     border: Border.all(
+          //       color: Colors.black,
+          //       width: 1,
+          //     ),
+          //   ),
+          //   child: TextButton(
+          //     onPressed: () {
+          //       showImageSourceSelection();
+          //     },
+          //     child: const Center(
+          //       child: Text('Capture Image'),
+          //     ),
+          //   ),
+          // ),
         ],
       ),
     );
